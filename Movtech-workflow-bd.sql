43c1434948bd5e0b153772e5d272e5fd60d590f2
Gustavo Kauan <gustavokauan2002@hotmail.com>
2023-06-05 12:01:05 -0300

    no message

--- Movtech-workflow-bd.sql
+++ Movtech-workflow-bd.sql
@@ -1,7 +1,7 @@
 use movtech;
 
 create table MvtCadEtapas (
-	codEtapas int not null primary key,
+	codEtapas int identity primary key,
 	etapas varchar(50) not null,
 	seqOrdem int not null,
 	leadTime int not null
